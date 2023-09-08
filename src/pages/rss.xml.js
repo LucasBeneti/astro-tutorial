b2a9aa1c3f16fbc0026560e67de2a16142f81d6e
Lucas Beneti <lbeneti@gmail.com>
2023-09-08 18:18:28 -0300

    Switched from file base routing to content pattern

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET() {
+    const posts = await getCollection('posts');
     return rss({
         title: 'Astro Learner | Blog',
         description: 'My journey learning Astro',
         site: 'https://lucasbeneti-astro-blog-tutorial.netlify.app',
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+        items: posts.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            link: `/posts/${post.slug}`,
+        })),
         customData: `<language>en-us</language>`,
     });
 }
